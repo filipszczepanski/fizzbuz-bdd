75c15412d8cd3916f38bc4c206d9a25c89d1f086
Filip Szczepanski <dev@szczepanski.us>
2018-08-01 16:46:49 +0200

    feat(node): Use terminal interface to play the game.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
-console.log('\n>>> FIZZ-BUZZ GAME <<<');
-console.log('>>> END GAME <<< \n');
+const args = process.argv.slice(2);
+const FizzBuzz = require('./fizz-buzz');
+
+const colors = {
+  'Fizz': '\x1b[35m',
+  'Buzz': '\x1b[36m',
+  'FizzBuzz': '\x1b[33m'
+};
+
+try {
+  const game = new FizzBuzz(...args);
+  const results = game.play().map(value =>
+    (colors[value] || '') + value + '\x1b[0m'
+  );
+
+  console.log('\n>>> FIZZ BUZZ GAME <<<');
+  console.log(results.join(' | '));
+  console.log('>>> END GAME <<< \n');
+
+} catch (error) {
+  console.error(error);
+}
